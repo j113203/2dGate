7e2a6986e4a092eb44c984b005e3311cf21468f7
j113203 <j113203@gmail.com>
2016-02-28 20:43:36 +0800

    //

--- Dev/2dgate.js
+++ Dev/2dgate.js
@@ -13,12 +13,33 @@ var $2dgate = {
 					callback(data.query.results.tr);
 				});
 			}
+		},
+		full : function(callback){
+			$.get(YQL_robot("http://anime.2d-gate.org/__cache.html"),function(data){
+				var rows = [];
+				$.each($(data.query.results.resources.content).filter("table#animeList").children("tbody").children(), function(i , n){
+					var $row = $(n);
+					rows.push({
+						"標題" : $row.find('td:eq(6)').html(),
+						"年份" : $row.find('td:eq(1)').text(),
+						"季節" : $row.find('td:eq(2)').text(),
+						"瀏覽數" : $row.find('td:eq(3)').text(),
+						"發表於" : $row.find('td:eq(4)').text(),
+						"更新於" : $row.find('td:eq(5)').text(),
+						"連結" :  $row.find('td:eq(0) a').attr('href')
+					});
+				});
+				return callback(rows);
+			});
 		}
-	},
+	}
 };
 var YQL = function(url , xpath){
 	xpath = xpath || "*" ;
-	return "//query.yahooapis.com/v1/public/yql?q="+ encodeURIComponent('select * from html where url="' + url + '" and xpath="'+ xpath +'"')+"&format=json&callback=";
+	return "//query.yahooapis.com/v1/public/yql?q="+ encodeURIComponent('select * from html where url="' + url + '" and xpath="'+ xpath +'"')+"&format=json";
+}
+var YQL_robot = function(url){
+	return "//query.yahooapis.com/v1/public/yql?q="+ encodeURIComponent('select content from data.headers where url="' + url+'"')+"&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 }
 
 
